db06c4396a34cb06bf353bd4f4141086beaa0b61
YusufBeraSERT <zesert2141@gmail.com>
2024-08-20 15:09:18 +0300

    Update ilk.py

--- ilk.py
+++ ilk.py
@@ -2,6 +2,7 @@ meme_dict = {
             "CRINGE": "Garip ya da utandırıcı bir şey",
             "LOL": "Komik bir şeye verilen cevap",
             "HELLO": "Selam verme eylemine denir",
+            "TETRIS": "bazı şekilli parçaların birbirleri ile doğru birleşmesi ile kazanılan birer oyun",
             }
             
 word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
